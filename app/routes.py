e5f5c7499942024bd7924db341939b2149cb0771
DmitrySevostianov <dmitry.sevostianov.mephi@yandex.ru>
2018-04-15 21:50:16 +0300

    /home added

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from app import app 
 
 @app.route('/')
+@app.route('/home')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
