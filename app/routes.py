cfe2314a4f9bf8733d2cfb886d2b3e26fa4ce1c8
DmitrySevostianov <dmitry.sevostianov.mephi@yandex.ru>
2018-04-17 17:34:59 +0300

    wrong beh.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+
 from app import app 
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -28,8 +30,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 
 @app.route('/logout')
 def logout():
